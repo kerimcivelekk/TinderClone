dfd44fab1e011c08b8278b3fd92c2efda93d0c32
Kerim <targetbad@hotmail.com>
2022-05-23 15:38:29 +0300

    Initial Commit

---
+++ SwiftUI TinderClone/SwiftUI_TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TinderCloneApp.swift
+//  SwiftUI TinderClone
+//
+//  Created by Kerim Civelek on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
